8706312fb1a538c6854bae2ab7d2b1de696a52ce
kunitl <kunitl@gmail.com>
2025-09-07 21:25:36 +0800

    feat: add button to set grid size (max 100)
    - Add a button at the top that prompts for squares per side.
    - Rebuild the grid in the same fixed space (960px).
    - Limit input to 100 to prevent performance issues.

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-const container = document.querySelector(".container");
+const boardSize = 480;
+let numSquaresPerSide = 16;
 
-for (let i = 0; i < 256; i++) {
-  const squareDiv = document.createElement("div");
-  squareDiv.classList.add("square");
-  container.appendChild(squareDiv);
-  squareDiv.addEventListener("mouseenter", () => squareDiv.classList.add("bgColor"));
+const container = document.querySelector(".container");
+container.style.width = `${boardSize}px`;
+
+function createSquareDivs() {
+  for (let i = 0; i < numSquaresPerSide ** 2; i++) {
+    const squareDiv = document.createElement("div");
+    squareDiv.classList.add("square");
+    squareDiv.style.width = `${boardSize / numSquaresPerSide}px`;
+    squareDiv.style.height = `${boardSize / numSquaresPerSide}px`;
+    squareDiv.addEventListener("mouseenter", () => squareDiv.classList.add("bgColor"));
+    container.appendChild(squareDiv);
+  }
 }
 
+const reset = document.querySelector("#reset");
+
+reset.addEventListener("click", () => {
+  while (container.lastChild) {
+    container.removeChild(container.lastChild);
+  }
+  while (true) {
+    let input = prompt(
+      "Enter the number of squares per side (max 100).\nPress 'Cancel' to use the default value: 16.",
+      "16"
+    );
+    if (input === null) {
+      numSquaresPerSide = 16;
+      break;
+    } else if (isNaN(input) || parseInt(input) > 100) {
+      alert("Please enter a number with maximum of 100");
+      continue;
+    } else {
+      numSquaresPerSide = parseInt(input);
+      break;
+    }
+  }
+  createSquareDivs();
+});
+
+createSquareDivs();
